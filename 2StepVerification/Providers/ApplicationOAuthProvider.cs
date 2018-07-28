2bc331ba92c48c733dad95e1c2b8124a5fb9b196
Asmaa <a.elbasioni@gmail.com>
2018-07-28 22:42:55 +0200

    feat: Add a test databese, OAuth provider, & readme file
    - Use Sql server express edition to create a database in DataDirectory
    - Populate the database with a sample of data
    - Add a class that represents OAuth provider that:
            * validate the credentials for users
            * and generate the token

---
+++ 2StepVerification/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace _2StepVerification.Providers
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //generate randum number of 6 digits to be used as verification code
+            Random generator = new Random();
+            String verificationCode = generator.Next(1111, 1000000).ToString("D6");
+
+            int affectedRows = 0;
+            string queryString = @"Update dbo.Users set [VerificationCode]=@VerificationCode , VerificationCodeDate = getdate() where [Email] = @UserEmail and [Password] = @UserPassword;
+	                                    select @@ROWCOUNT";
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@UserEmail", context.UserName);
+                command.Parameters.AddWithValue("@UserPassword", context.Password);
+                command.Parameters.AddWithValue("@VerificationCode", verificationCode);
+
+                try
+                {
+                    connection.Open();
+                    affectedRows = Convert.ToInt16(command.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    context.SetError("sql_error", ex.Message);
+                    return;
+                }
+                finally {
+                    if (connection.State != ConnectionState.Closed)
+                        connection.Close();
+                }
+            }
+            
+
+            if (affectedRows > 0)
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Email, context.UserName));
+                
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");                
+                return;
+            }
+
+          
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // Resource owner password credentials does not provide a client ID.
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            //if (context.ClientId == _publicClientId)
+            //{
+            Uri expectedRootUri = new Uri(context.Request.Uri, "/");
+
+            if (expectedRootUri.AbsoluteUri == context.RedirectUri)
+            {
+                context.Validated();
+            }
+            //}
+
+            return Task.FromResult<object>(null);
+
+        }
+
+        public static AuthenticationProperties CreateProperties(string userName)
+        {
+            IDictionary<string, string> data = new Dictionary<string, string>
+            {
+                { "userName", userName }
+            };
+            return new AuthenticationProperties(data);
+        }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# 2 Step verification application
+
+It's an asp.net web application showing how to secure ASP.NET Web API using Token Based Authentication and 2 step verification **without** using asp.net identity 
+
+
+In a real application, you should store the connection string securely, as described in [Connection Strings and Configuration Files](https://msdn.microsoft.com/en-us/library/ms254494.aspx).
