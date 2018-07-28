34b9bbe112e3ac1cf07612db10a7a8c1679e3146
Asmaa <a.elbasioni@gmail.com>
2018-07-29 01:45:45 +0200

    - feat: Add and configure OWIN startup class

---
+++ 2StepVerification/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+using _2StepVerification.Providers;
+
+[assembly: OwinStartup(typeof(_2StepVerification.Startup))]
+
+namespace _2StepVerification
+{
+    public class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var myProvider = new ApplicationOAuthProvider();
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(4),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
