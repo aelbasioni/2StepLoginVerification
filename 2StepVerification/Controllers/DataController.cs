400f64c0d9412a957bdf3c77e0476a7cd74b470b
Asmaa <a.elbasioni@gmail.com>
2018-07-29 02:27:55 +0200

    feat: Add web api controllers
    - One controller that is responsible for the second step verification
    - One controller that has an anonymous action and a secured one for test

---
+++ 2StepVerification/Controllers/DataController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _2StepVerification.Controllers
+{
+    public class DataController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/FetchPublicData")]
+        public IHttpActionResult GetPublicData()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+
+        [Authorize(Roles = "verifieduser")]
+        [HttpGet]
+        [Route("api/FetchSecuredData")]
+        public IHttpActionResult GetSecuredData()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
---
+++ 2StepVerification/Controllers/SecondStepVerificationController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Web.Http;
+using System.Security.Claims;
+using System.Data;
+using Microsoft.Owin.Security;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _2StepVerification.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Auth")]
+    public class SecondStepVerificationController : ApiController
+    {
+        [Authorize(Roles = "user")]
+        [Route("Validate")]
+        [HttpGet]
+        public IHttpActionResult ValidateCode(string verificationCode)
+        {
+            string accessToken = "";
+            string Email = "", Name = "";
+            DataTable dt = new DataTable();
+
+            if (!string.IsNullOrWhiteSpace(verificationCode))
+            {
+            
+                var identity = (ClaimsIdentity)User.Identity;
+                IEnumerable<Claim> claims = identity.Claims;
+
+                
+                string email = claims.Where(c => c.Type == ClaimTypes.Email).Select(c => c.Value).SingleOrDefault();
+
+                /*
+                 * the part 
+                 * DATEDIFF(MINUTE, GETDATE(), VerificationCodeDate)<=10 
+                 * in the queryString means that if the VerificationCodeDate passes 10 minutes it'll be invalid, 
+                 * and the user neads to re-login
+                 */
+                string queryString  = @"SELECT FullName,Email from dbo.Users where [Email] = @UserEmail and VerificationCode = @VerificationCode and  DATEDIFF(MINUTE, GETDATE(), VerificationCodeDate)<=10; ";
+
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.AddWithValue("@UserEmail", email);
+                    command.Parameters.AddWithValue("@VerificationCode", verificationCode);
+
+                    SqlDataAdapter sda = new SqlDataAdapter(command);
+                    sda.Fill(dt);                    
+                }
+
+                if (dt.Rows.Count > 0)
+                {
+                    Email = Convert.ToString(dt.Rows[0]["Email"]);
+                    Name = Convert.ToString(dt.Rows[0]["UserName"]);
+
+                    identity.AddClaim(new Claim(ClaimTypes.Role, "verifieduser")); //sub role                   
+                    identity.AddClaim(new Claim(ClaimTypes.Name, Name));
+                    identity.AddClaim(new Claim(ClaimTypes.Email, Email));
+                   
+                    AuthenticationTicket ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+
+                    DateTime currentUtc = DateTime.UtcNow;
+                    ticket.Properties.IssuedUtc = currentUtc;
+                    ticket.Properties.ExpiresUtc = currentUtc.Add(Startup.OAuthOptions.AccessTokenExpireTimeSpan);
+
+                    accessToken = Startup.OAuthOptions.AccessTokenFormat.Protect(ticket);
+                   
+                }
+                else
+                {
+                    throw new Exception("Invalid Code");
+                }
+            }
+
+            return Ok(new { accessToken = accessToken, name = Name, email = Email});
+        }
+
+    }
+}
